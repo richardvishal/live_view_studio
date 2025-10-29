aca9400ef2b3e0f4503df4befbe1ef8bab55ae2a
richie <richie@ardhika.com>
2025-10-29 11:11:15 +0530

    sales_live starter file

--- lib/live_view_studio_web/live/sales_live.ex
+++ lib/live_view_studio_web/live/sales_live.ex
@@ -4,11 +4,13 @@ defmodule LiveViewStudioWeb.SalesLive do
   alias LiveViewStudio.Sales
 
   def mount(_params, _session, socket) do
-    if connected?(socket) do
-      :timer.send_interval(1000, self(), :tick)
-    end
+    socket =
+      assign(socket,
+        new_orders: Sales.new_orders(),
+        sales_amount: Sales.sales_amount(),
+        satisfaction: Sales.satisfaction()
+      )
 
-    socket = assign_stats(socket)
     {:ok, socket}
   end
 
@@ -43,28 +45,10 @@ defmodule LiveViewStudioWeb.SalesLive do
         </div>
       </div>
 
-      <button phx-click="refresh">
+      <button>
         <img src="/images/refresh.svg" /> Refresh
       </button>
     </div>
     """
-  end
-
-  def handle_event("refresh", _, socket) do
-    socket = assign_stats(socket)
-    {:noreply, socket}
-  end
-
-  def handle_info(:tick, socket) do
-    socket = assign_stats(socket)
-    {:noreply, socket}
-  end
-
-  def assign_stats(socket) do
-    assign(socket,
-      new_orders: Sales.new_orders(),
-      sales_amount: Sales.sales_amount(),
-      satisfaction: Sales.satisfaction()
-    )
   end
 end
