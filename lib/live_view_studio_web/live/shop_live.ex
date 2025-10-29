1995f7f292353a4ad530c826da485cb98d14e236
richie <richie@ardhika.com>
2025-10-29 13:54:06 +0530

    shop_live starter file

--- lib/live_view_studio_web/live/shop_live.ex
+++ lib/live_view_studio_web/live/shop_live.ex
@@ -2,7 +2,6 @@ defmodule LiveViewStudioWeb.ShopLive do
   use LiveViewStudioWeb, :live_view
 
   alias LiveViewStudio.Products
-  alias Phoenix.LiveView.JS
 
   def mount(_params, _session, socket) do
     {:ok,
@@ -13,27 +12,13 @@ defmodule LiveViewStudioWeb.ShopLive do
      )}
   end
 
+  def handle_event("toggle-show-cart", _, socket) do
+    socket = update(socket, :show_cart, fn show -> !show end)
+    {:noreply, socket}
+  end
+
   def handle_event("add-product", %{"product" => product}, socket) do
     cart = Map.update(socket.assigns.cart, product, 1, &(&1 + 1))
     {:noreply, assign(socket, :cart, cart)}
-  end
-
-  def toggle_cart() do
-    JS.toggle(
-      to: "#cart",
-      in: {"easi-in-out duration-300", "translate-x-full", "translate-x-0"},
-      out: {"easi-in-out duration-300", "translate-x-0", "translate-x-full"},
-      time: 300
-    )
-    |> JS.toggle(
-      to: "#backdrop",
-      in: "fade-in",
-      out: "fade-out"
-    )
-  end
-
-  def add_product(product) do
-    JS.push("add-product", value: %{product: product.image})
-    |> JS.transition("shake", to: "#cart-button", time: 500)
   end
 end
