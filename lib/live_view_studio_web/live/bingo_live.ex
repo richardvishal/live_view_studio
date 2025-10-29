06081c6754ab977d66e779cd7881f218cacf82c3
richie <richie@ardhika.com>
2025-10-29 14:34:12 +0530

    bingo_live starter file

--- lib/live_view_studio_web/live/bingo_live.ex
+++ lib/live_view_studio_web/live/bingo_live.ex
@@ -1,30 +1,11 @@
 defmodule LiveViewStudioWeb.BingoLive do
   use LiveViewStudioWeb, :live_view
 
-  alias LiveViewStudioWeb.Presence
-
-  @topic "users:playing-bingo"
   def mount(_params, _session, socket) do
-    %{current_user: current_user} = socket.assigns
-
-    if connected?(socket) do
-      Presence.subscribe(@topic)
-
-      {:ok, _} =
-        Presence.track_user(current_user, @topic, %{
-          arrived_at: Timex.now() |> Timex.format!("%H:%M", :strftime)
-        })
-
-      :timer.send_interval(3000, self(), :call_number)
-    end
-
-    presences = Presence.list(@topic)
-
     socket =
       assign(socket,
         number: nil,
-        numbers: all_numbers(),
-        presences: Presence.simple_presence_map(presences)
+        numbers: all_numbers()
       )
 
     {:ok, socket}
@@ -35,41 +16,13 @@ defmodule LiveViewStudioWeb.BingoLive do
     <h1>Bingo Boss 📢</h1>
     <div id="bingo">
       <div class="users">
-        <ul>
-          <.presence :for={{_user_id, meta} <- @presences} meta={meta} />
-        </ul>
+        <ul></ul>
       </div>
       <div class="number">
         <%= @number %>
       </div>
     </div>
     """
-  end
-
-  def presence(assigns) do
-    ~H"""
-    <li>
-      <span class="username">
-        <%= @meta.username %>
-      </span>
-      <span class="timestamp">
-        <%= @meta.arrived_at %>
-      </span>
-    </li>
-    """
-  end
-
-  # Assigns the next random bingo number, removing it
-  # from the assigned list of numbers. Resets the list
-  # when the last number has been picked.
-  def pick(socket) do
-    case socket.assigns.numbers do
-      [head | []] ->
-        assign(socket, number: head, numbers: all_numbers())
-
-      [head | tail] ->
-        assign(socket, number: head, numbers: tail)
-    end
   end
 
   # Returns a list of all valid bingo numbers in random order.
@@ -82,13 +35,5 @@ defmodule LiveViewStudioWeb.BingoLive do
       Enum.map(numbers, &"#{letter} #{&1}")
     end)
     |> Enum.shuffle()
-  end
-
-  def handle_info(:call_number, socket) do
-    {:noreply, pick(socket)}
-  end
-
-  def handle_info(%{event: "presence_diff", payload: diff}, socket) do
-    {:noreply, Presence.handle_diff(socket, diff)}
   end
 end
