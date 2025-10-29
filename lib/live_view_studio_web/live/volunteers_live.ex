98f7397f70d0d04ad8c14b7ed547b59f5bfe3929
richie <richie@ardhika.com>
2025-10-29 11:50:44 +0530

    volunteer_live starter file

--- lib/live_view_studio_web/live/volunteers_live.ex
+++ lib/live_view_studio_web/live/volunteers_live.ex
@@ -2,20 +2,13 @@ defmodule LiveViewStudioWeb.VolunteersLive do
   use LiveViewStudioWeb, :live_view
 
   alias LiveViewStudio.Volunteers
-  # alias LiveViewStudio.Volunteers.Volunteer
-  alias LiveViewStudioWeb.VolunteerFormComponent
 
   def mount(_params, _session, socket) do
-    if connected?(socket) do
-      Volunteers.subscribe()
-    end
-
     volunteers = Volunteers.list_volunteers()
 
     socket =
       socket
-      |> stream(:volunteers, volunteers)
-      |> assign(:count, length(volunteers))
+      |> assign(:volunteers, volunteers)
 
     {:ok, socket}
   end
@@ -24,86 +17,25 @@ defmodule LiveViewStudioWeb.VolunteersLive do
     ~H"""
     <h1>Volunteer Check In</h1>
     <div id="volunteer-checkin">
-      <.live_component
-        module={VolunteerFormComponent}
-        id={:new}
-        count={@count}
-      />
-      <div id="volunteers" phx-update="stream">
-        <.volunteer
-          :for={{volunteer_id, volunteer} <- @streams.volunteers}
-          volunteer={volunteer}
-          id={volunteer_id}
-        />
-      </div>
-    </div>
-    """
-  end
-
-  def volunteer(assigns) do
-    ~H"""
-    <div
-      class={"volunteer #{if @volunteer.checked_out, do: "out"}"}
-      id={@id}
-    >
-      <div class="name">
-        <%= @volunteer.name %>
-      </div>
-      <div class="phone">
-        <%= @volunteer.phone %>
-      </div>
-      <div class="status">
-        <button phx-click="toggle-status" phx-value-id={@volunteer.id}>
-          <%= if @volunteer.checked_out,
-            do: "Check In",
-            else: "Check Out" %>
-        </button>
-      </div>
-      <.link
-        class="delete"
-        phx-click="delete"
-        phx-value-id={@volunteer.id}
-        data-confirm="Are you sure?"
+      <div
+        :for={volunteer <- @volunteers}
+        class={"volunteer #{if volunteer.checked_out, do: "out"}"}
       >
-        <.icon name="hero-trash-solid" />
-      </.link>
+        <div class="name">
+          <%= volunteer.name %>
+        </div>
+        <div class="phone">
+          <%= volunteer.phone %>
+        </div>
+        <div class="status">
+          <button>
+            <%= if volunteer.checked_out,
+              do: "Check In",
+              else: "Check Out" %>
+          </button>
+        </div>
+      </div>
     </div>
     """
-  end
-
-  def handle_event("toggle-status", %{"id" => id}, socket) do
-    volunteer = Volunteers.get_volunteer!(id)
-
-    {:ok, _volunteer} = Volunteers.toggle_status_volunteer(volunteer)
-
-    {:noreply, socket}
-  end
-
-  def handle_event("delete", %{"id" => id}, socket) do
-    volunteer = Volunteers.get_volunteer!(id)
-    {:ok, _} = Volunteers.delete_volunteer(volunteer)
-    {:noreply, socket}
-  end
-
-  def handle_info({:volunteer_created, volunteer}, socket) do
-    socket =
-      stream_insert(socket, :volunteers, volunteer, at: 0)
-      |> update(:count, &(&1 + 1))
-
-    {:noreply, socket}
-  end
-
-  def handle_info({:volunteer_updated, volunteer}, socket) do
-    socket = stream_insert(socket, :volunteers, volunteer)
-
-    {:noreply, socket}
-  end
-
-  def handle_info({:volunteer_deleted, volunteer}, socket) do
-    socket =
-      stream_delete(socket, :volunteers, volunteer)
-      |> update(:count, &(&1 - 1))
-
-    {:noreply, socket}
   end
 end
